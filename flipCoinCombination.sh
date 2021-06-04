924598a56cd0ac7310721c95df7cdeac43ca6bf3
vismayatk002 <vismayatk002@gmail.com>
2021-06-05 00:06:11 +0530

    Updated with Percentage of Triplet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -16,16 +16,19 @@ for ((i=0; i<50; i++))
 do
 	random1=$((RANDOM%2))
 	random2=$((RANDOM%2))
+	random3=$((RANDOM%2))
 	randomC1=$(characterConvert $random1)
 	randomC2=$(characterConvert $random2)
-	if [ -v count[$randomC1$randomC2] ]
+	randomC3=$(characterConvert $random3)
+	if [ -v count[$randomC1$randomC2$randomC3] ]
 	then
-		((count[$randomC1$randomC2]++))
+		((count[$randomC1$randomC2$randomC3]++))
 	else
-		count[$randomC1$randomC2]=1
+		count[$randomC1$randomC2$randomC3]=1
 	fi
 done
 for key in "${!count[@]}"
 do
 	echo "Percentage of $key =" $((${count[$key]} * 100 / 50))
+	echo ${count[$key]}
 done
