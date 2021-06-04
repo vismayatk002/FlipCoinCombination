ef3fe7c1197b7cb0a714437d95a70ca39457bbfb
vismayatk002 <vismayatk002@gmail.com>
2021-06-04 23:56:24 +0530

    Updated with percentage of Doublet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,19 +2,31 @@
 declare -A count
 head=1
 tail=0
-count[head]=0
-count[tail]=0
+function characterConvert(){
+	
+	if [ $1 -eq 1 ]
+	then
+		echo "H"
+	else
+		echo "T"
+	fi
+
+}
 for ((i=0; i<50; i++))
 do
-	randomnumber=$((RANDOM%2))
-	if [ $randomnumber -eq $head ]
+	random1=$((RANDOM%2))
+	random2=$((RANDOM%2))
+	randomC1=$(characterConvert $random1)
+	randomC2=$(characterConvert $random2)
+	if [ -v count[$randomC1$randomC2] ]
 	then
-		((count[head]++))
+		((count[$randomC1$randomC2]++))
 	else
-		((count[tail]++))
+		count[$randomC1$randomC2]=1
 	fi
 done
 for key in "${!count[@]}"
 do
 	echo "Percentage of $key =" $((${count[$key]} * 100 / 50))
+	echo ${count[$key]}
 done
