a7a639218dc6402f3308c01d2ecd18509e2ab75a
vismayatk002 <vismayatk002@gmail.com>
2021-06-04 23:00:55 +0530

    Updated with Percentage of Head and Tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
+declare -A count
 head=1
 tail=0
-randomnumber=$((RANDOM%2))
-if [ $randomnumber -eq $head ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+count[head]=0
+count[tail]=0
+for ((i=0; i<50; i++))
+do
+	randomnumber=$((RANDOM%2))
+	if [ $randomnumber -eq $head ]
+	then
+		((count[head]++))
+	else
+		((count[tail]++))
+	fi
+done
+for key in "${!count[@]}"
+do
+	echo "Percentage of $key =" $((${count[$key]} * 100 / 50))
+done
